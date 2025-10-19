2123e183a3f862e6943633344ebdb9efe925d93f
ka1yan <changkaiyan@come-future.com>
2025-10-19 18:49:59 +0800

    feature:nacos config

--- services/service-order/src/main/java/com/ka1yan/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/ka1yan/order/controller/OrderController.java
@@ -3,17 +3,30 @@ package com.ka1yan.order.controller;
 import com.ka1yan.order.entity.Order;
 import com.ka1yan.order.service.OrderService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
     @Resource
     private OrderService orderService;
+
+    @Value("${order.timeout}")
+    private Integer orderTimeout;
+    @Value("${order.auto-confirm}")
+    private Integer autoConfirm;
+
+    @GetMapping("/prop")
+    public String  getProperties() {
+        return "order.timeout:" + orderTimeout + "; auto-confirm:" + autoConfirm;
+    }
 
     @GetMapping("/create")
     public Order createOrder(@RequestParam Long userId,
